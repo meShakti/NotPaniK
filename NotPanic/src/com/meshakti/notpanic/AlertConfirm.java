b6bbf4e9451b7a18e547013bd5eae33cf9f4a912
Shakti <svpsctg11@yahoo.com>
2014-09-07 20:43:36 +0530

    added missing alertconfirm

---
+++ NotPanic/src/com/meshakti/notpanic/AlertConfirm.java
@@ -0,0 +1,25 @@
+package com.meshakti.notpanic;
+
+import android.app.IntentService;
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+
+public class AlertConfirm extends IntentService {
+
+	public AlertConfirm() {
+		super("AlertConfirm");
+
+	}
+
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		AudioManager listener = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+		if (listener.getRingerMode() == AudioManager.RINGER_MODE_NORMAL) {
+			Intent i = new Intent(AlertReciever.TRIGER_COMMAND);
+			sendBroadcast(i);
+		}
+		stopSelf();
+	}
+
+}
